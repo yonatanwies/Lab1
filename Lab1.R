cbccfee9b5815771ec97066b294f94eb2d3f6b92
yonatanwies <yonatanwies@gmail.com>
2023-05-11 13:33:24 +0300

    Added few lines of code

--- Lab1.R
+++ Lab1.R
@@ -1,4 +1,10 @@
-library(tidyverse)
-url <-"https://en.wikipedia.org/wiki/Democracy_Index"
 #To update our file, first save the changes locally on your computer, then click on the GIT button (next to "go to file/function)
 # and click "commit". this will save the branch. Afterwards click on "Push" to merge changes to main file.
+
+library(tidyverse)
+library(rvest)
+url <-"https://en.wikipedia.org/wiki/Democracy_Index"
+democracy.index<-read_html(url)
+
+list.by.region<- democracy.index %>% html_table()
+head(list.by.region)
