9159620d0ad937456ceb886d530a88fedc1199d1
yonatanwies <yonatanwies@gmail.com>
2023-05-11 13:19:17 +0300

    Updated instructions on how to commit changes.

--- Lab1.R
+++ Lab1.R
@@ -1,5 +1,4 @@
 library(tidyverse)
 url <-"https://en.wikipedia.org/wiki/Democracy_Index"
-
-#This is a revision of Lab1 13:10
-yonatan <-c(1:5)
+#To update our file, first save the changes locally on your computer, then click on the GIT button (next to "go to file/function)
+# and click "commit". this will save the branch. Afterwards click on "Push" to merge changes to main file.
