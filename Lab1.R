5c4cf2357834938ef846d70a862e5e7ee47bdfd5
yonatanwies <yonatanwies@gmail.com>
2023-05-21 14:36:21 +0300

    solution to 2.a.ii

--- Lab1.R
+++ Lab1.R
@@ -9,26 +9,23 @@ library(reshape2) # melt: change data-frame format long/wide
 library(e1071) # skewness and kurtosis
 library(rvest)
 #1.a
+all.tables = html_nodes(source, "table")  
 
+# Use html_table to extract the individual tables from the all.tables object:
 url = 'https://en.wikipedia.org/wiki/Democracy_Index'
 source = read_html(url)
-tables<-html_table(source)
-list.by.region<-tables[5]
-list.by.country<-tables[7]
-components <- tables[8]
-tables[1]
 list.by.region <- source %>%
   html_nodes(xpath = '//*[@id="mw-content-text"]/div[1]/table[4]') %>%
-  html_table()
+  html_table(fill =TRUE)
 list.by.region <- as.data.frame(list.by.region[[1]])
-#list.by.country <- source %>%
-#  html_nodes(xpath = '//*[@id="mw-content-text"]/div[1]/table[6]') %>%
-#  html_table(fill = TRUE)
-list.by.country <- as.data.frame(list.by.country)
-#components <- source %>%
-#  html_nodes(xpath = '//*[@id="mw-content-text"]/div[1]/table[7]') %>%
-#  html_table()
-components <- as.data.frame(components)
+list.by.country <- source %>%
+  html_nodes(xpath = '//*[@id="mw-content-text"]/div[1]/table[6]') %>%
+  html_table(fill = TRUE)
+list.by.country <- as.data.frame(list.by.country[[1]])
+components <- source %>%
+  html_nodes(xpath = '//*[@id="mw-content-text"]/div[1]/table[7]') %>%
+  html_table()
+components <- as.data.frame(components[[1]])
 head(list.by.region, 5)
 head(list.by.country, 5)
 head(components, 5)
@@ -42,11 +39,47 @@ top_five <- list.by.country %>%
   arrange(`2022 rank`) %>% 
   select(`Country`, `2022 rank`) %>% 
   head(5)
-
+x<-c(list.by.country[5,which(list.by.country$'Region'=="North America")])
+x
 avg.list<-rowMeans(list.by.country[5:length(list.by.country)]) %>% sort(decreasing = TRUE)
 top.average.five<-avg.list[1:5]
 bottom.average.five<-sort(avg.list,decreasing = FALSE)[1:5]
 
-#2.
+#2.a
 
 boxplot(list.by.country$"2022" ~ list.by.country$Region)
+#i want to create a data frame that the name is the region's name, and the value is the vector of
+# values of 2022 for each region
+regions<- list.by.country$Region %>% unique()
+
+combined_list <- vector("list", length(regions))
+for (i in 1:length(regions)) {
+  region <- regions[i]
+  data_vec <- list.by.country$"2022"[which(list.by.country$Region == region)]
+  combined_list[[i]] <- data_vec
+}
+for(i in 1:length(regions)){
+  if(boxplot.stats(combined_list[[i]])[4]!="numeric(0)"){
+    for(j in boxplot.stats(combined_list[[i]])[4]){
+      print(list.by.country$Country[which(list.by.country$"2022"==j)])
+    }
+  }
+}
+combined_list[[3]]
+boxplot.stats(combined_list[[2]])[4] == "numeric(0)"
+
+#2.b
+ggplot(list.by.country, aes(x = `2022`, fill = Region)) +
+  geom_density(alpha = 0.5) +
+  labs(x = "Democracy Index (2022)", y = "Density") +
+  ggtitle("Density Plots of Democracy Index by Region (2022)")
+summary_stats <- list.by.country %>%
+  group_by(Region) %>%
+  summarise(
+    Mean = mean(`2022`),
+    Variance = var(`2022`),
+    Skewness = skewness(`2022`),
+    Kurtosis = kurtosis(`2022`)
+  )
+
+print(summary_stats)
