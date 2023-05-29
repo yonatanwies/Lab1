8f8247fd9dfed7a6cc6522f2b945d55014b43067
Rom2712 <rom2712@gmail.com>
2023-05-29 15:52:30 +0300

    rom hamelech

--- Lab1.R
+++ Lab1.R
@@ -9,17 +9,18 @@ library(reshape2) # melt: change data-frame format long/wide
 library(e1071) # skewness and kurtosis
 library(rvest)
 #1.a
-all.tables = html_nodes(source, "table")  
+
 
 # Use html_table to extract the individual tables from the all.tables object:
 url = 'https://en.wikipedia.org/wiki/Democracy_Index'
 source = read_html(url)
+all.tables = html_nodes(source, "table")  
 list.by.region <- source %>%
   html_nodes(xpath = '//*[@id="mw-content-text"]/div[1]/table[4]') %>%
   html_table(fill =TRUE)
 list.by.region <- as.data.frame(list.by.region[[1]])
 list.by.country <- source %>%
-  html_nodes(xpath = '//*[@id="mw-content-text"]/div[1]/table[6]') %>%
+  html_nodes(xpath = '//*[@id="mw-content-text"]/div[1]/table[5]') %>%
   html_table(fill = TRUE)
 list.by.country <- as.data.frame(list.by.country[[1]])
 components <- source %>%
@@ -40,7 +41,7 @@ top_five <- list.by.country %>%
   select(`Country`, `2022 rank`) %>% 
   head(5)
 x<-c(list.by.country[5,which(list.by.country$'Region'=="North America")])
-x
+list.by.country
 avg.list<-rowMeans(list.by.country[5:length(list.by.country)]) %>% sort(decreasing = TRUE)
 top.average.five<-avg.list[1:5]
 bottom.average.five<-sort(avg.list,decreasing = FALSE)[1:5]
@@ -48,8 +49,27 @@ bottom.average.five<-sort(avg.list,decreasing = FALSE)[1:5]
 #2.a
 
 boxplot(list.by.country$"2022" ~ list.by.country$Region)
-#2.b
+#i want to create a data frame that the name is the region's name, and the value is the vector of
+# values of 2022 for each region
+regions<- list.by.country$Region %>% unique()
 
+combined_list <- vector("list", length(regions))
+for (i in 1:length(regions)) {
+  region <- regions[i]
+  data_vec <- list.by.country$"2022"[which(list.by.country$Region == region)]
+  combined_list[[i]] <- data_vec
+}
+for(i in 1:length(regions)){
+  if(boxplot.stats(combined_list[[i]])[4]!="numeric(0)"){
+    for(j in boxplot.stats(combined_list[[i]])[4]){
+      print(list.by.country$Country[which(list.by.country$"2022"==j)])
+    }
+  }
+}
+combined_list[[3]]
+boxplot.stats(combined_list[[2]])[4] == "numeric(0)"
+
+#2.b
 ggplot(list.by.country, aes(x = `2022`, fill = Region)) +
   geom_density(alpha = 0.5) +
   labs(x = "Democracy Index (2022)", y = "Density") +
@@ -76,9 +96,7 @@ plot_democracy_index_country <- function(data, names) {
     geom_line() +
     labs(x = "Year", y = "Democracy Index", color = grouping_column, linetype = grouping_column) +
     ggtitle("Democracy Index Over Time") +
-    theme(legend.title = element_blank())
-    
-}
+    theme(legend.title = element_blank())}
 
 #A function for listbyregion
 
@@ -97,22 +115,185 @@ plot_democracy_index_region <- function(data, names) {
 plot_democracy_index_region(list.by.region,region_vector)
 plot_democracy_index_country(list.by.country,c("England","Israel","Iraq"))
 #3b
-plot_democracy_index_clusters <- function(data) {
-  data$Change <- data$`2022` - data$`2006`
-  cluster1 <- data[data$Change >= 1.5, ]
-  cluster2 <- data[data$Change <= -1.5, ]
-  cluster3 <- data[data$Change >= 0.75 & data$Change <= 1.5, ]
-  cluster4 <- data[data$Change <= -0.75 & data$Change >= -1.5, ]
-  cluster5 <- data[data$Change <= -0.75 & data$`2022` - data$`Lowest drop` >= 0.75, ]
-  cluster6 <- data[data$Change >= 0.75 & data$`2022` - data$`Highest point` <= -0.75, ]
-  cluster7 <- data[data$`Highest` - data$`Lowest` < 0.5, ]
-  cluster8 <- data[!(data %in% rbind(cluster1, cluster2, cluster3, cluster4, cluster5,cluster6,cluster7)), ]
+list.by.country$Change <- list.by.country$`2022` - list.by.country$`2006`
+cluster1 <- list.by.country[list.by.country$Change >= 1.5, ]
+cluster2 <- list.by.country[list.by.country$Change <= -1.5, ]
+cluster3 <- list.by.country[list.by.country$Change >= 0.75 & list.by.country$Change <= 1.5, ]
+clust5<-filter(list.by.country,'2006'-apply(list.by.country[,6:18],1,min)>=0.75 & '2022'-apply(list.by.country[,6:18],1,min)>=0.75)
+cluster4 <- list.by.country[list.by.country$Change <= -0.75 & list.by.country$Change >= -1.5, ]
+cluster5 <- list.by.country[list.by.country$Change <= -0.75 & list.by.country$`2022` - list.by.country$`Lowest drop` >= 0.75, ]
+cluster6 <- list.by.country[list.by.country$Change >= 0.75 & list.by.country$`2022` - list.by.country$`Highest point` <= -0.75, ]
+cluster7 <- list.by.country[list.by.country$`Highest` - list.by.country$`Lowest` < 0.5, ]
+cluster8 <- list.by.country[!(list.by.country %in% rbind(cluster1, cluster2, cluster3, cluster4, cluster5,cluster6,cluster7)), ]
 }
-  plot_democracy_index_country(cluster1[,-length(cluster1)],cluster1$Country)
-  plot_democracy_index_country(cluster2[,-length(cluster2)], cluster2$Country)
-  plot_democracy_index_country(cluster3[,-length(cluster3)], cluster3$Country)
-  plot_democracy_index_country(cluster4[,-length(cluster4)], cluster4$Country)
-  plot_democracy_index_country(cluster5[,-length(cluster5)], cluster5$Country)
-  plot_democracy_index_country(cluster6[,-length(cluster6)], cluster6$Country)
-  plot_democracy_index_country(cluster7[,-length(cluster7)], cluster7$Country)
-  plot_democracy_index_country(cluster8[,-length(cluster8)], cluster8$Country)
+plot_democracy_index_country(cluster1[,-length(cluster1)],cluster1$Country)
+plot_democracy_index_country(cluster2[,-length(cluster2)], cluster2$Country)
+plot_democracy_index_country(cluster3[,-length(cluster3)], cluster3$Country)
+plot_democracy_index_country(cluster4[,-length(cluster4)], cluster4$Country)
+plot_democracy_index_country(cluster5[,-length(cluster5)], cluster5$Country)
+plot_democracy_index_country(cluster6[,-length(cluster6)], cluster6$Country)
+plot_democracy_index_country(cluster7[,-length(cluster7)], cluster7$Country)
+plot_democracy_index_country(cluster8[,-length(cluster8)], cluster8$Country)
+
+
+
+#4
+#helper function to determine type of regime for each country.
+whichDemocracy<-function(num){
+  ifelse(num>8 & num <=10,"Full democracy",
+         ifelse(num>6 & num<=8, "Flawed democracy",
+                ifelse(num>4 & num<=6,"Hybrid Regime","Authoritarian")))
+  
+}
+data_2006 <- list.by.country$"2006"
+data_2022 <- list.by.country$"2022"
+
+empirical_changes<-function(old,new){
+  regime.type<-c("Full democracy","Flawed democracy","Hybrid Regime","Authoritarian")
+  result<-data.frame(matrix(0,nrow=4,ncol = 4,dimnames = c(regime.type %>% list(),regime.type %>% list())))
+  for(i in 1:length(regime.type)){
+    for(j in 1:length(regime.type)){
+      to<-list.by.country$Country[which(whichDemocracy(data_2006)==regime.type[i])]
+      from<-list.by.country$Country[which(whichDemocracy(data_2022)==regime.type[j])]
+      val<-length(to[to%in%from])/length(to) 
+      result[i,j]<-val
+      
+    }
+  }
+  return(result)
+}
+
+empirical_changes(data_2006,data_2022)
+names <- c("Full democracy", "Flawed democracy", "Flawed democracy", "Authoritarian")
+
+# Create the heatmap
+heatmap(empirical_changes(data_2006,data_2022)%>%as.matrix(),
+        Rowv = NA,      # Disable row clustering
+        Colv = NA,      # Disable column clustering
+        scale = "none", # Do not scale the values
+        main = "Empirical Changes",
+        colnames = names,
+        rownames = names)
+
+
+
+#Q5
+#GDP
+url = "https://en.wikipedia.org/wiki/List_of_countries_by_GDP_(PPP)_per_capita"
+source = read_html(url)
+all.tables = html_nodes(source, "table")  
+gdp.countries <- source %>%
+  html_nodes(xpath = '//*[@id="mw-content-text"]/div[1]/table[1]') %>%
+  html_table(fill =TRUE)
+gdp.countries <- as.data.frame(gdp.countries[[1]])
+gdp.countries
+#INCARNATION
+url = "https://en.wikipedia.org/wiki/List_of_countries_by_incarceration_rate"
+source = read_html(url)
+all.tables = html_nodes(source, "table")  
+incarnation.rates <- source %>%
+  html_nodes(xpath = '//*[@id="mw-content-text"]/div[1]/table[2]') %>%
+  html_table(fill =TRUE)
+incarnation.rates <- as.data.frame(incarnation.rates[[1]])
+#AREA
+
+url = "https://en.wikipedia.org/wiki/List_of_countries_and_dependencies_by_area"
+source = read_html(url)
+all.tables = html_nodes(source, "table")  
+area <- source %>%
+  html_nodes(xpath = '//*[@id="mw-content-text"]/div[1]/table[2]') %>%
+  html_table(fill =TRUE)
+area <- as.data.frame(area[[1]])
+
+#POPULATION SIZE
+url = "https://en.wikipedia.org/wiki/List_of_countries_and_dependencies_by_population"
+source = read_html(url)
+all.tables = html_nodes(source, "table")  
+population <- source %>%
+  html_nodes(xpath = '//*[@id="mw-content-text"]/div[1]/table[2]') %>%
+  html_table(fill =TRUE)
+population <- as.data.frame(population)
+#Change the name of the column country
+colnames(area)[2] = "Country"
+colnames(population)[2] = "Country"
+colnames(incarnation.rates)[1] = "Country"
+colnames(gdp.countries)[1] = "Country"
+#clean the names
+incarnation.rates$Country= gsub("\\*", "", incarnation.rates$Country)
+incarnation.rates$Country= gsub("\\ ","", incarnation.rates$Country)
+incarnation.rates$Country= gsub("\\s", "", incarnation.rates$Country)
+incarnation.rates$Country = gsub("\\[Note\\]", "", incarnation.rates$Country)
+gdp.countries$Country= gsub("\\*", "", gdp.countries$Country)
+gdp.countries$Country= gsub("\\ ","", gdp.countries$Country)
+gdp.countries$Country= gsub("\\s","", gdp.countries$Country)
+area$Country= gsub("\\ ", "", area$Country)
+area$Country= gsub("\\(.*\\)", "", area$Country)
+area$Country= gsub("\\s", "", area$Country)
+population$Country= gsub("\\ ", "", population$Country)
+population$Country= gsub("\\(.*\\)", "", population$Country)
+population$Country= gsub("\\s", "", population$Country)
+list.by.country$Country=gsub("\\ ", "", list.by.country$Country)
+list.by.country$Country=gsub("\\s", "", list.by.country$Country)
+list.by.country$Country= gsub("\\(.*\\)", "", list.by.country$Country)
+names(gdp.countries)
+view(merged_df)
+# Merge the two dataframes based on the common columns
+merged_df <- merge(list.by.country, population, by = "Country",by.y = "Country", all = TRUE)
+merged_df <- merge(merged_df, area, by = "Country",by.y = "Country / Dependency", all = TRUE)
+merged_df <- merge(merged_df, incarnation.rates, by = "Country",by.y = "Location", all = TRUE)
+merged_df <- merge(merged_df, gdp.countries, by = "Country",by.y = "Country/Territory", all = TRUE)
+combined.table.GI=merge(gdp.countries,incarnation.rates)
+combined.table.GIA=merge(combined.table.GI,area)
+combined.table.GIAP=merge(combined.table.GIA,population,by="Country")
+combined.table.GIAPL=merge(combined.table.GIAP,list.by.country,by="Country")
+# Print the merged dataframe
+head(merged_df,5)
+view(combined.table.GIAPL)
+
+
+
+#5.b.
+#Subset the dataframe to include only the necessary columns
+data_subset <- merged_df[, c("CIA[8][9][10]", "2022 rank")]
+data_subset%>%head()
+x# Remove any rows with missing values
+data_subset <- na.omit(data_subset)
+data_subset$`CIA[8][9][10]` <- as.numeric(data_subset$`CIA[8][9][10]`)
+
+# Fit a linear regression model
+model <- lm(`CIA[8][9][10]` ~ `2022`, data = data_subset)
+# Print the model summary
+summary(model)
+
+
+#6.a.
+# Compute the empirical CDF
+combined.table.GIAPL$`CIA[8][9][10]`<-as.numeric(gsub("[^0-9.]","",combined.table.GIAPL$`CIA[8][9][10]`))
+gdp=ecdf(combined.table.GIAPL$`CIA[8][9][10]`)
+plot(gdp, main = "Empirical CDF of GDP (PPP) per capita", xlab = "GDP (PPP) per capita", ylab = "Empirical CDF")
+
+#6.b.
+combined.table.GIAPL$Population <- as.numeric(gsub("[^0-9.]","",combined.table.GIAPL$Population))
+combined.table.GIAPL$`CIA[8][9][10]`<-as.numeric(gsub("[^0-9.]","",combined.table.GIAPL$`CIA[8][9][10]`))
+gdp_per_capita <-combined.table.GIAPL$Population / combined.table.GIAPL$`CIA[8][9][10]`
+gdp_per_capita.sort=sort(gdp_per_capita)
+empirical_probs <- cumsum(combined.table.GIAPL$Population) / sum(combined.table.GIAPL$Population)
+plot(gdp_per_capita.sort, empirical_probs, type = "s", main = "Empirical CDF of GDP per capita by population)",
+     xlab = "GDP per capita", ylab = "Cumulative-Probability")
+
+#7.
+avg.list2<-rowMeans(list.by.country[5:length(list.by.country)])
+map_list=bind_cols(list.by.country$Country,avg.list2)
+colnames(map_list)[colnames(map_list) == "...1"] <- "Country"
+colnames(map_list)[colnames(map_list) == "...2"] <- "average_democracy_index"
+install.packages("rworldmap") 
+library(rworldmap)
+data("countryExData", package = "rworldmap")
+map_list_clean <- map_list[complete.cases(map_list$average_democracy_index), ]
+world_map <- joinCountryData2Map(map_list_clean, joinCode = "ISO3", nameJoinColumn = "Country", verbose = TRUE)
+par(mar = c(0, 0, 0, 0))
+mapCountryData(world_map, nameColumnToPlot = "average_democracy_index", mapRegion = "world", colourPalette = "heat",
+               addLegend = TRUE, borderCol = "grey", mapTitle = "Average Democracy Index (2006-2022)",
+               aspect = 1, missingCountryCol = NA, add = FALSE, nameColumnToHatch = "", lwd = 0.5)
+mapCountryData(world_map, nameColumnToPlot = "average_democracy_index", catMethod = "fixedWidth",
+               mapTitle = "Average Democracy Index (2006-2022)", addLegend = TRUE)
